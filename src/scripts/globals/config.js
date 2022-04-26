ed21974ab3b9834b119db23764638804f6de4cc1
Mas 'Edi <aslekmak54@gmail.com>
2022-04-26 10:00:31 +0700

    request API

---
+++ src/scripts/globals/config.js
@@ -0,0 +1,8 @@
+const CONFIG = {
+    KEY: 'd6499c2e7f98ed4557949c0699263a43',
+    BASE_URL: 'https://api.themoviedb.org/3/',
+    BASE_IMAGE_URL: 'https://image.tmdb.org/t/p/w500/',
+    DEFAULT_LANGUAGE: 'en-us',
+  };
+   
+  export default CONFIG;
