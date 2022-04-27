ff668924aadc544156556402524f5d81e286d4df
Mas 'Edi <aslekmak54@gmail.com>
2022-04-27 13:08:45 +0700

    update

--- src/scripts/globals/config.js
+++ src/scripts/globals/config.js
@@ -1,12 +1,12 @@
 const CONFIG = {
-    KEY: 'd6499c2e7f98ed4557949c0699263a43',
-    BASE_URL: 'https://api.themoviedb.org/3/',
-    BASE_IMAGE_URL: 'https://image.tmdb.org/t/p/w500/',
-    DEFAULT_LANGUAGE: 'en-us',
-    CACHE_NAME: new Date().toISOString(),
-    DATABASE_NAME: 'movie-catalogue-database',
-    DATABASE_VERSION: 1,
-    OBJECT_STORE_NAME: 'movies',
-  };
-   
-  export default CONFIG;
+  KEY: 'YOUR_API_KEY',
+  BASE_URL: 'https://api.themoviedb.org/3/',
+  BASE_IMAGE_URL: 'https://image.tmdb.org/t/p/w500/',
+  DEFAULT_LANGUAGE: 'en-us',
+  CACHE_NAME: 'MovieCatalogue-V1',
+  DATABASE_NAME: 'movie-catalogue-database',
+  DATABASE_VERSION: 1,
+  OBJECT_STORE_NAME: 'movies',
+};
+
+export default CONFIG;
--- src/scripts/views/app.js
+++ src/scripts/views/app.js
@@ -1,23 +1,23 @@
 import DrawerInitiator from '../utils/drawer-initiator';
 import UrlParser from '../routes/url-parser';
 import routes from '../routes/routes';
- 
+
 class App {
   constructor({ button, drawer, content }) {
     this._button = button;
     this._drawer = drawer;
     this._content = content;
- 
+
     this._initialAppShell();
   }
- 
+
   _initialAppShell() {
     DrawerInitiator.init({
       button: this._button,
       drawer: this._drawer,
       content: this._content,
     });
- 
+
     // kita bisa menginisiasikan komponen lain bila ada
   }
 
@@ -28,5 +28,5 @@ class App {
     await page.afterRender();
   }
 }
- 
+
 export default App;
